395bfbbfbf37ae9ef45f1cd94eb184ed0748188b
LivStokes <Oliviastokess02@gmail.com>
2025-10-08 17:18:39 +0100

    Add files via upload

--- Week2/Pset2/scrabble.c
+++ Week2/Pset2/scrabble.c
@@ -1,21 +1,49 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+
+int wordScore(string word);
+
+int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int main(void)
 {
     // Prompt input
-    string s = get_string("Input: ");
-    printf("Player 1: ");
-    string s = get_string("Input: ");
-    printf("Player 2: ");
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score of words
+    int score1 = wordScore(word1);
+    int score2 = wordScore(word2);
+
+    // Score Output
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
-
-
-
-// prompt for input twice: once for “Player 1” to input their word and once for “Player 2” to input their word. Then, depending on which player scores the most points, your program should either print “Player 1 wins!”, “Player 2 wins!”, or “Tie!” 
-// Should have 2 prompts, 3 outputs
-// Player 1: Question?
-// Player 2: Question!
-// Tie!
+// Scoring Function
+int wordScore(string word)
+{
+    // Array of points in order
+    int pointSum = 0;
+    for (int i = 0; i < strlen(word); i++)
+    {
+        if (isalpha(word[i]))
+        {
+            int letters = toupper(word[i]) - 'A';
+            pointSum += points[letters];
+        }
+    }
+    return pointSum;
+}
