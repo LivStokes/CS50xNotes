a48911013ed6d8ea5d31b41b1e795c7c5b333b3a
LivStokes <Oliviastokess02@gmail.com>
2025-11-27 13:35:40 +0000

    Add files via upload

---
+++ Week7/Lecture7/favourites.py
@@ -0,0 +1,14 @@
+import csv
+
+with open("favourites.csv", "r") as file:
+    reader = csv.DictReader(file)
+    counts = {}
+    for row in reader:
+        favourite = row["language"]
+        if favourite in counts:
+            counts[favourite] += 1
+        else:
+            counts[favourite] = 1
+
+for favourite in sorted(counts, key=counts.get, reverse=True):
+    print(f"{favourite}: {counts[favourite]}")
